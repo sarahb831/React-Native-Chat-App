2e1b6ba502f455fbd3e0fd72efed6881cf9e6087
SBeauchamp <smbeauchamp@mindspring.com>
2019-08-28 09:29:09 -0700

    add navigate support

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// import the screens
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+// import React Navigation
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+//create navigator for mapping to these screens
+const navigator = createStackNavigator({
+  Start: {screen: Start},
+  Chat: {screen: Chat}
+});
+
+// create app container to manage app state and link main nav to app environment
+const NavigatorContainer = createAppContainer(navigator);
+
+// export it as default navigator container
+export default NavigatorContainer;
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      < NavigatorContainer />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ components/Chat.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Image, StyleSheet, TextInput, View } from 'react-native';
+//import NavigatorContainer from '../App';
+
+export default class Chat extends Component {
+     
+  constructor(props) {
+    super(props);
+    this.state={ name: ''}
+  }
+
+  render() {
+    return (
+        <View style={styles.container}>
+            
+                <Text>Welcome to the Chat screen!</Text>
+            //
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background: {
+      width: '100%',
+      height: '100%',
+  },
+  title: {
+      fontSize: 45,
+      fontWeight: '600',
+      color: '#757083',
+      opacity: .5,
+  },
+  startBox: {
+      backgroundColor: 'white',
+      width: '88%',
+      height: '44%',
+      alignItems: 'flex-end',
+      justifyContent: 'center',
+
+  },  
+  textInput: {
+      fontSize: 16,
+      fontWeight: '300',
+      color: '#757083',
+      opacity: 1,
+  },
+  colorBox: {
+      fontSize: 16,
+      fontWeight: '300',
+      color: '#757083',
+      opacity: 1
+  },
+  colorCircles: {
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  circle1RadioButton: {
+      backgroundColor: '#090C08',
+  },
+  circle2RadioButton: {
+      backgroundColor: '#474056',
+  },
+  circle3RadioButton: {
+      backgroundColor: '#8A95A5',
+  },
+  circle4RadioButton: {
+      backgroundColor: '#B9C6AE',
+  },
+  button: {
+      fontSize: 16,
+      fontWeight: '600',
+      color: '#757083',
+    },
+});
