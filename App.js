29776598aec8a6890864536fc1d04e6857ad4b58
SBeauchamp <smbeauchamp@mindspring.com>
2019-10-13 12:49:36 -0700

    add jsdoc and eslint support

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 /**
- * 
- * This project uses the React Native framework, GiftedChat library, and 
+ *  This project uses the React Native framework, GiftedChat library, and 
  * Firestore/Firebase to create a chat application which allows a user to
  * select a background color, sign up, create and receive messages, take
  * or select a picture and send it, and send their location to others in
@@ -10,8 +9,8 @@
  * Chat screen.
  * 
  * Start.js contains the Start class, which allows the user to enter their user name and select a background
- * color.
- * 
+ * * color.
+
  * Chat.js contains the Chat class, which handles all of the chat screen functions except where it calls
  * those from CustomActions.js.
  * 
@@ -31,10 +30,10 @@ import Chat from './components/Chat';
 // import React Navigation
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-//create navigator for mapping to these screens
+// create navigator for mapping to these screens
 const navigator = createStackNavigator({
-  Start: {screen: Start},
-  Chat: {screen: Chat} 
+  Start: { screen: Start },
+  Chat: { screen: Chat } 
 });
 
 // create app container to manage app state and link main nav to app environment
@@ -43,8 +42,7 @@ const NavigatorContainer = createAppContainer(navigator);
 // export it as default navigator container
 export default NavigatorContainer;
 
-class App extends Component {
-  
+class App extends Component { 
   constructor(props) {
     super(props);
   }
